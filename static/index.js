8393b54f012b42594ec0d3bff86dc2b72a47e802
Trexlo <zprotuli@gmail.com>
2024-07-02 15:31:54 +0200

    task-2: added average review function, remove button and remove review function

--- static/index.js
+++ static/index.js
@@ -4,10 +4,15 @@ let mockReviews = [
         rating: 5
     },
     {
+        content: "Lorem ipsum!",
+        rating: 5
+    },
+    {
         content: "Dolor sit amet.",
         rating: 4
     }
 ]
+const movieAverageRating = document.getElementById("average-rating");
 const reviewContainer = document.getElementById("review-container");
 const reviewForm = document.getElementById("review-form");
 const reviewFormText = document.getElementById("review-text-input");
@@ -21,9 +26,17 @@ function createReviewElement(review) {
     const reviewRating = document.createElement('p');
     reviewRating.classList.add('review-rating');
     reviewRating.textContent = review.rating + " / 5";
-
+    const reviewButton = document.createElement('p');
+    reviewButton.classList.add('review-button');
+    reviewButton.textContent = "Remove";
+    reviewButton.onclick = () => {
+        mockReviews = mockReviews.filter(r=> r !=review);
+        reviewElement.remove();
+        calculateAverageRating();
+    }
     reviewElement.appendChild(reviewText);
     reviewElement.appendChild(reviewRating);
+    reviewElement.appendChild(reviewButton);
     return reviewElement;
 }
 
@@ -36,14 +49,22 @@ reviewForm.onsubmit = (ev)=>{
     }
     const newReview = {
         content: content,
-        rating: rating,
+        rating: parseInt(rating),
     };
     mockReviews.push(newReview)
+    calculateAverageRating();
     reviewContainer.appendChild(createReviewElement(newReview));
 }
-
+function calculateAverageRating(){
+    if(mockReviews.length>0){
+        movieAverageRating.textContent = ((mockReviews.reduce((sum, r)=> r.rating + sum, 0)/mockReviews.length)).toFixed(1) + " / 5";
+    }else{
+        movieAverageRating.textContent = "0 / 5";
+    }
+}
 function loadMockReviews(){
     console.log(mockReviews);
+    calculateAverageRating();
     mockReviews.forEach(review => reviewContainer.appendChild(createReviewElement(review)));
 }
 loadMockReviews();
