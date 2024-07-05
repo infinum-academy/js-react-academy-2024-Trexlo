ca04a6d8d3ad0589b1e8d132fdf31874aaa08187
Trexlo <zprotuli@gmail.com>
2024-07-05 14:47:51 +0200

    added typings (IShow), added component ShowDetails

--- tv-shows-app/src/app/page.tsx
+++ tv-shows-app/src/app/page.tsx
@@ -1,7 +1,17 @@
+import { ShowDetails } from "@/components/features/shows/ShowDetails/ShowDetails";
 import styles from "./page.module.css";
+import { IShow } from "@/typings/Show.type";
 
 export default function Home() {
+  const mockShowDetails: IShow = {
+    averageRating: undefined,
+    description: "A chemistry teacher diagnosed with inoperable lung cancer turns to manufacturing and selling methamphetamine with a former student in order to secure his family's future.",
+    imageUrl: "https://e1.pxfuel.com/desktop-wallpaper/31/210/desktop-wallpaper-8-breaking-bad-heisenberg.jpg",
+    title: "Breaking Bad"
+  }
   return (
-    <main className={styles.main}></main>
+    <main className={styles.main}>
+      <ShowDetails {...mockShowDetails}></ShowDetails>
+    </main>
   );
 }
---
+++ tv-shows-app/src/components/features/shows/ShowDetails/ShowDetails.tsx
@@ -0,0 +1,18 @@
+import { IShow } from "@/typings/Show.type";
+import { Card, CardHeader, CardBody, CardFooter, Stack } from '@chakra-ui/react'
+import { Flex, Heading, Image, Text } from "@chakra-ui/react";
+
+export const ShowDetails = (showData: IShow) =>{
+    return (
+        <Card overflow={"hidden"} w={["100%", "100%", "65%"]} rounded={20} backgroundColor={"indigo"} color={"white"}>
+            <Image alt="Show image" src={showData.imageUrl || "https://fakeimg.pl/600x400?text=Show+Image"}></Image>
+            <CardBody>
+                <Stack mt='6' spacing='4'>
+                    <Heading size={"lg"}>{showData.title}</Heading>
+                    <Text>{showData.description}</Text>
+                    <Text>{(showData.averageRating && (showData.averageRating + " / 5")) || "No ratings"}</Text>
+                </Stack>
+            </CardBody>
+        </Card>
+    );
+}
