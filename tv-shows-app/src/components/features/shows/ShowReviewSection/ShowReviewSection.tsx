e5b5c5fa9bc1e14c81721fc46e0fed95d02be9ff
Trexlo <zprotuli@gmail.com>
2024-07-05 17:49:56 +0200

    task2: localStorage support

--- tv-shows-app/src/components/features/shows/ShowReviewSection/ShowReviewSection.tsx
+++ tv-shows-app/src/components/features/shows/ShowReviewSection/ShowReviewSection.tsx
@@ -1,49 +1,67 @@
 'use client';
 import { IReview } from "@/typings/Review.type";
 import { ReviewList } from "../../review/ReviewList/ReviewList";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { ReviewForm } from "../ReviewForm/ReviewForm";
 import { Flex } from "@chakra-ui/react";
 
 interface IShowReviewSectionProps{
     updateRating: (rating:number) => void;
 }
+    
+var mockReviews: IReview[]=[
+    {
+        avatar: "https://fakeimg.pl/100x100?text=:)",
+        comment: "Some comment",
+        email: "email@email.com",
+        rating: 3
+    },
+    {
+        avatar: "https://fakeimg.pl/30x30?text=:)",
+        comment: "Some comment 2 ",
+        email: "email@email.com",
+        rating: 1
+    },
+    {
+        avatar: "https://fakeimg.pl/50x50?text=:)",
+        comment: "Some comment 5",
+        email: "email@email.com",
+        rating: 5
+    }
+]
 
 export const ShowReviewSection = ({updateRating}:IShowReviewSectionProps) =>{
-    
-    var mockReviews: IReview[]=[
-        {
-            avatar: "https://fakeimg.pl/100x100?text=:)",
-            comment: "Some comment",
-            email: "email@email.com",
-            rating: 3
-        },
-        {
-            avatar: "https://fakeimg.pl/30x30?text=:)",
-            comment: "Some comment 2 ",
-            email: "email@email.com",
-            rating: 1
-        },
-        {
-            avatar: "https://fakeimg.pl/50x50?text=:)",
-            comment: "Some comment 5",
-            email: "email@email.com",
-            rating: 5
-        }
-    ]
-    
+
     const [reviews, setReviews] = useState(mockReviews);
+
+    const loadReviewsFromLocalStorage = ():IReview[] => {
+        const reviewsString = localStorage.getItem('reviews');
+        if(!reviewsString){
+            return [];
+        }
+        return JSON.parse(reviewsString) as IReview[];
+    }
+
     useEffect(()=>{
-        updateRating(reviews.reduce((sum, r)=> r.rating + sum, 0)/reviews.length);
-    }, [reviews, updateRating]);
-    
+        setReviews(loadReviewsFromLocalStorage);
+    }, []);
+
+    const saveReviewsToLocalStorage = (reviews:IReview[])=>{
+        localStorage.setItem('reviews', JSON.stringify(reviews));
+    };
+
     const removeReview = (review:IReview) => {
         const newReviews = reviews.filter(r => r != review);    
         setReviews(newReviews); 
+        updateRating(reviews.reduce((sum, r)=> r.rating + sum, 0)/reviews.length);
+        saveReviewsToLocalStorage(newReviews);
     }
 
     const addReview = (review:IReview) => {
-        setReviews([review, ...reviews]);     
+        const newReviews = [review, ...reviews];
+        setReviews(newReviews);     
+        updateRating(reviews.reduce((sum, r)=> r.rating + sum, 0)/reviews.length);
+        saveReviewsToLocalStorage(newReviews);
     }
 
     return (
