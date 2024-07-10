c2a35d8e0d7186df07331a71efcd6a2360d08a58
Trexlo <zprotuli@gmail.com>
2024-07-10 11:21:08 +0200

    task-2: added fetchers

---
+++ tv-shows-app/src/fetchers/show.ts
@@ -0,0 +1,16 @@
+import { fetcher } from '@/fetchers/fetcher';
+import { IShow } from '@/typings/Show.type';
+
+interface IShowListsResponse {
+	shows: Array<IShow>;
+}
+
+export function getAllShows() {
+	return fetcher<IShowListsResponse>('/api/shows');
+}
+export function getTopShows() {
+	return fetcher<IShowListsResponse>('/api/shows/top-rated');
+}
+export function getShow(id: string) {
+	return fetcher<IShow>(`/api/shows/${id}`);
+}
