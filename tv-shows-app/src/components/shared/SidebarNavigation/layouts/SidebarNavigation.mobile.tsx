d66334668fa8ec26f2b4d09235355c23875e8704
Trexlo <zprotuli@gmail.com>
2024-07-20 15:23:13 +0200

    added hamburger icon, added logo image

--- tv-shows-app/src/components/shared/SidebarNavigation/layouts/SidebarNavigation.mobile.tsx
+++ tv-shows-app/src/components/shared/SidebarNavigation/layouts/SidebarNavigation.mobile.tsx
@@ -1,22 +1,37 @@
 "use client"    
 import { navigationItems } from "@/app/data/navigation-items";
-import { Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, useDisclosure } from "@chakra-ui/react"
+import { Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, Flex, Icon, IconButton, Image, useDisclosure } from "@chakra-ui/react"
 import NextLink from 'next/link';
 import { usePathname } from "next/navigation";
-
+import Logo from "../../../../assets/icons/logo.svg";
+import { HamburgerIcon } from "@chakra-ui/icons";
 export const SidebarNavigationMobile = () => {
     const pathname = usePathname();
     const { isOpen, onOpen, onClose } = useDisclosure();
-    
+
     return (<>
-      <Button colorScheme='blue' onClick={onOpen}>
-        Open
-      </Button>
+
+      <Flex 
+        flexDirection={"row"} 
+        justifyContent={"space-between"} 
+        padding={"17px"}
+        alignItems={"center"}
+      >
+        <Image src="/images/logo.svg"/>
+        <HamburgerIcon
+          w={"24px"}
+          h={"24px"}
+          color={"white"}
+          aria-label='Menu'
+          onClick={onOpen}
+        />
+      </Flex>
+
       <Drawer placement={"right"} onClose={onClose} isOpen={isOpen}>
         <DrawerOverlay />
         <DrawerContent borderTopLeftRadius={"16px"} bg={"purple"}>
           <DrawerHeader>
-            <DrawerCloseButton />
+            <DrawerCloseButton color={"white"}/>
           </DrawerHeader>
           <DrawerBody>
                 {
